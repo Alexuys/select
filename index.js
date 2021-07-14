f3c1ddfa6fdc1e3747d99275a06f5b121566cbfd
Alex <59532969+Alexuys@users.noreply.github.com>
2021-07-14 15:27:37 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const select = new Select('.select', {
+        placeholder: 'Default text',
+        data: [
+            {id: '1', value: 'React'},
+            {id: '2', value: 'Vue'},
+            {id: '3', value: 'Angular'},
+            {id: '4', value: 'Next'},
+            {id: '5', value: 'NodeJs'},
+        ],
+        selectedId: '1'
+    })
+
+
+select
